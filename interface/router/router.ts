a6f080d8852d97c157a6d044a4c96b1b90089b1f
Hiroto Toyoda <>
2023-06-26 17:09:13 +0900

    Add: 必要なエンドポイントを定義する

---
+++ interface/router/router.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+
+app.route('/users')
+    .post((req, res) => {
+        
+    });
+
+app.route('/login')
+    .post((req, res) => {
+
+    });
+
+app.route('/tasks')
+    .post((req, res) => {
+
+    })
+    .get((req, res) => {
+
+    })
+    .put((req, res) => {
+
+    });
