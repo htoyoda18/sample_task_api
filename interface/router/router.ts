d0f7eb1a2fdafe2ce998c0f8f817d4cb6e643c1d
Hiroto Toyoda <>
2023-06-27 21:31:22 +0900

    Add: morganを使い、リクエストのログを記録する

--- interface/router/router.ts
+++ interface/router/router.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.route('/users')
     .post((req, res) => {
-        
+
     });
 
 app.route('/login')
