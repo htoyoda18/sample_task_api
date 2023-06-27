124574f3abd64469713ef7f7dc899aab2fc1cfc5
Hiroto Toyoda <>
2023-06-27 20:52:38 +0900

    fix: jwtの生成する際に、try catch構文を使うようにする

--- shared/auth.ts
+++ shared/auth.ts
@@ -6,19 +6,31 @@ interface TokenPayload {
     userID: string;
 }
 
-export const generateToken = (userID: string) => {
+export const generateToken = (userID: string): string => {
     const payload: TokenPayload = {
-        userID: userID
+        userID
     }
-    const token = jwt.sign(payload, envConfig.JWT_KEY, { expiresIn: envConfig.JWT_TOKEN_EXPIRE, algorithm: 'RS256' });
-    return token;
+
+    try {
+        const token = jwt.sign(payload, envConfig.JWT_KEY, { expiresIn: envConfig.JWT_TOKEN_EXPIRE, algorithm: 'HS256' });
+        return token;
+    } catch (error) {
+        console.error("Error generating JWT token:", error);
+        throw new Error(ErrorType.FailGeneratingJWT);
+    }
 }
 
-export const parseToken = (token: string) => {
-    const decoded = jwt.verify(token, envConfig.JWT_KEY) as TokenPayload;
-    if (decoded && decoded.userID) {
-        return decoded.userID;
-    } else {
+export const parseToken = (token: string): string => {
+    try {
+        const decoded = jwt.verify(token, envConfig.JWT_KEY) as TokenPayload;
+
+        if (decoded && decoded.userID) {
+            return decoded.userID;
+        } else {
+            throw new Error(ErrorType.InvalidToken);
+        }
+    } catch (error) {
+        console.error("Error parsing JWT token:", error);
         throw new Error(ErrorType.InvalidToken);
     }
 }
--- shared/error.ts
+++ shared/error.ts
@@ -1,4 +1,5 @@
 export const ErrorType = {
     InvalidToken: 'Invalid token',
+    FailGeneratingJWT: 'Fail generating JWT',
     MissingEnvironmentVariable: 'Missing environment variable',
 }
