ecb82c353a3eac09f16ad1872395d4b59f7bde7d
Hiroto Toyoda <>
2023-06-27 21:48:01 +0900

    Add: winstonを使用して、ログを扱えるようにした

---
+++ shared/logger.ts
@@ -0,0 +1,19 @@
+import { createLogger, format, transports } from 'winston';
+
+export const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.colorize(),
+        format.json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' })
+    ]
+});
